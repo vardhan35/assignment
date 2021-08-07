96b22d2796f3ebc9bea6da849c90db77bacb214d
vardhan <harshwardhankalse03@gmail.com>
2021-08-07 10:55:08 +0530

    importing css and routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Header from "./components/Header";
 import "./app.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app">
-      {/* <Home /> */}
-      <Login />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
