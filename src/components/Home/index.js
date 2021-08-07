49e14a251e77372039a26506f279957969ad006b
vardhan <harshwardhankalse03@gmail.com>
2021-08-07 10:56:34 +0530

    importin css in home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import "./home.css";
 
 const Homepage = () => {
   return (
     <div className="homePage">
-      <h1>Home page</h1>
-      <button>Logout</button>
+      <h1 className="homeTitle">Welcome Back, Ramesh</h1>
+      <h2 className="homeSubTitle">Your Details are as follows</h2>
+      <div className="userDetails">
+        <p className="homeDetails">Name : Ramesh Gupta</p>
+        <p className="homeDetails">Email : rameshgupta@gmail.com</p>
+        <p className="homeDetails">Name : rameshgupta</p>
+      </div>
+      <button className="homeButton">Logout</button>
     </div>
   );
 };
