c40689736bf8b559d4fd63cf3d0b4e3d8be70fa7
vardhan <harshwardhankalse03@gmail.com>
2021-08-07 09:59:27 +0530

    Importing css into app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import "./app.css";
 
 const App = () => {
   return (
     <div className="app">
-      <Home />
+      {/* <Home /> */}
       <Login />
     </div>
   );
